daa4c496a5f4470f465398d2aa2c8610d4d1a885
unbanksytv <livethelife.tv@gmail.com>
2022-12-15 21:02:25 +0100

    update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ const metadata = {
     <meta name="description" content={description} />
   ),
   tagline: (tagline: string | undefined) => (
-    <>{tagline || 'Discover, buy and sell NFTs'}</>
+    <>{tagline || 'Odyssey Curated DAA'}</>
   ),
   image: (image?: string) => {
     if (image) {
@@ -89,7 +89,7 @@ const Home: NextPage<Props> = ({ fallback }) => {
       <div className="col-span-full px-6 md:px-16">
         <div className="mb-9 flex w-full items-center justify-between">
           <div className="reservoir-h4 dark:text-white">
-            Trending Collections
+            Top 50 Curated by LTL
           </div>
           {!isSmallDevice && <SortTrendingCollections />}
         </div>
